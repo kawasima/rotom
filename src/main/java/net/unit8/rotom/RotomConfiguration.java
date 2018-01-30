4d973d9a84bc3537bfe7ac3cd6da2bcd700666bf
miyamoen <akihito.biblioteca@gmail.com>
2018-01-30 13:57:55 +0900

    Add unauthenticated redirection

--- src/main/java/net/unit8/rotom/RotomConfiguration.java
+++ src/main/java/net/unit8/rotom/RotomConfiguration.java
@@ -1,13 +1,23 @@
 package net.unit8.rotom;
 
+
+import enkan.Endpoint;
 import enkan.component.ComponentLifecycle;
 import enkan.component.SystemComponent;
+import enkan.data.HttpRequest;
+import enkan.data.HttpResponse;
 import enkan.security.AuthBackend;
+import enkan.util.HttpResponseUtils;
 import net.unit8.rotom.middleware.backend.AnonymousBackend;
+
 
 public class RotomConfiguration extends SystemComponent {
     private String basePath = "";
     private AuthBackend<?, ?> authBackend = new AnonymousBackend();
+    private Endpoint<HttpRequest, HttpResponse> unauthEndpoint =
+            (Endpoint<HttpRequest, HttpResponse>) req ->
+                    HttpResponseUtils.redirect("/my/signIn?url=" + req.getUri(),
+                            HttpResponseUtils.RedirectStatusCode.TEMPORARY_REDIRECT);
 
     @Override
     protected ComponentLifecycle<RotomConfiguration> lifecycle() {
@@ -37,5 +47,13 @@ public class RotomConfiguration extends SystemComponent {
 
     public void setAuthBackend(AuthBackend<?, ?> authBackend) {
         this.authBackend = authBackend;
+    }
+
+    public Endpoint<HttpRequest, HttpResponse> getUnauthEndpoint() {
+        return unauthEndpoint;
+    }
+
+    public void setUnauthEndpoint(Endpoint<HttpRequest, HttpResponse> unauthEndpoint) {
+        this.unauthEndpoint = unauthEndpoint;
     }
 }
